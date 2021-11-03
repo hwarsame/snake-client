600c51e91bba8744905f069632c88349664fd9c0
Hamza Warsame <hamza360123@gmail.com>
2021-11-03 19:36:48 -0400

    export setupInput function

--- client.js
+++ client.js
@@ -1,14 +1,14 @@
 const net = require("net");
 
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: '165.227.47.243',// IP address here,
     port: '50541'// PORT number here,
   });
   conn.setEncoding("utf8");
   conn.on("connect", () => {
-    console.log('You have successfully connected to the server! :)')
+    console.log('You have successfully connected to the server! :)');
     conn.write("Name: BIG");
   });
   conn.on("connect", (data) =>{
@@ -16,9 +16,9 @@ const connect = function () {
     //   conn.write('Move: up')
     // }, 3000)
     
-  })
+  });
   return conn;
 };
 
 
-module.exports = connect
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,23 +1,8 @@
 // const net = require("net");
 // establishes a connection with the game server
-const connect = require('./client')
+const connect = require('./client');
+const setupInput = require('./input');
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  
-
-  const handleUserInput = function(key){
-    if (key === '\u0003') {
-      process.exit();
-    }
-  }
-  stdin.on('data', handleUserInput);
-  return stdin
-};
 console.log("Connecting ...");
 connect();
 setupInput();
