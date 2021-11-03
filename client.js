d3ad77ffc61bcb9bed6c164d90dd61622f27b8cb
Hamza Warsame <hamza360123@gmail.com>
2021-11-03 17:32:15 -0400

    use connect function as a module to connect to game

---
+++ client.js
@@ -0,0 +1,15 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',// IP address here,
+    port: '50541'// PORT number here,
+  });
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log('You have successfully connected to the server! :)')
+  });
+  return conn;
+};
+
+module.exports = connect
--- play.js
+++ play.js
@@ -1,19 +1,8 @@
-const net = require("net");
-
+// const net = require("net");
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',// IP address here,
-    port: '50541'// PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log('You have successfully connected to the server! :)')
-  });
-  return conn;
-};
-
+const connect = require('./client')
 console.log("Connecting ...");
 connect();
+
+
+
